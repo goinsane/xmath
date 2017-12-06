297ecc48a38831761d9b5eac92730d65aba62ed0
Orkun karaduman <orkunkaraduman@gmail.com>
2017-12-06 19:47:53 +0300

    Added Index, Each, Keys, Values functions in lazycore. Added examples about.

--- examples/lazycore.go
+++ examples/lazycore.go
@@ -15,6 +15,24 @@ func DuplicateTest() {
 	fmt.Printf("pDst := Duplicate(src).(*int64) -> src=%d(0x%x) dst=%d(0x%x)\n", src, pSrc, dst, pDst)
 }
 
+func IndexTest() {
+	s := []int{3, 4, 5, 6}
+	fmt.Printf("Slice is %v\n", s)
+	fmt.Printf("Index of 5 in slice: %d\n", lazycore.Index(s, 5))
+	fmt.Printf("Index of 1 in slice: %d\n", lazycore.Index(s, 1))
+}
+
+func EachTest() {
+	m := map[string]int{"a": 3, "b": 4, "c": 5, "d": 6}
+	fmt.Printf("Map is %v\n", m)
+	k, v := lazycore.Each(m)
+	fmt.Printf("Keys and values in map with Each function (ordered): %v %v\n", k, v)
+	k, v = lazycore.Keys(m), lazycore.Values(m)
+	fmt.Printf("Keys and values in map with Keys, Values functions: %v %v\n", k, v)
+}
+
 func main() {
 	DuplicateTest()
+	IndexTest()
+	EachTest()
 }
--- lazycore/lazycore.go
+++ lazycore/lazycore.go
@@ -3,6 +3,7 @@ package lazycore
 import (
 	"reflect"
 	"unsafe"
+	"github.com/pkg/errors"
 )
 
 // Duplicate returns the pointer of duplicated value of i.
@@ -18,4 +19,52 @@ func Duplicate(i interface{}) interface{} {
 		dst++
 	}
 	return r.Interface()
+}
+
+// Index returns the index s from v. If not found return -1. Panics v is not array or slice.
+func Index(v interface{}, s interface{}) int {
+	rv := reflect.ValueOf(v)
+	if k := rv.Type().Kind(); k == reflect.Ptr {
+		rv = reflect.Indirect(rv)
+	}
+	if k := rv.Type().Kind(); k != reflect.Array && k != reflect.Slice {
+		panic(errors.New("v must be Array or Slice"))
+	}
+	for i := 0; i < rv.Len(); i++ {
+		if reflect.DeepEqual(rv.Index(i).Interface(), s) {
+			return i
+		}
+	}
+	return -1
+}
+
+// Each returns the keys slice and values slice from v. Panics v is not map.
+func Each(v interface{}) ([]interface{}, []interface{}) {
+	rv := reflect.ValueOf(v)
+	if k := rv.Type().Kind(); k == reflect.Ptr {
+		rv = reflect.Indirect(rv)
+	}
+	if k := rv.Type().Kind(); k != reflect.Map {
+		panic(errors.New("v must be Map"))
+	}
+	var keys []interface{}
+	var vals []interface{}
+	rkeys := rv.MapKeys()
+	for _, rkey := range rkeys {
+		keys = append(keys, rkey.Interface())
+		vals = append(vals, rv.MapIndex(rkey).Interface())
+	}
+	return keys, vals
+}
+
+// Keys returns the keys slice from v. Panics v is not map.
+func Keys(v interface{}) []interface{} {
+	r, _ := Each(v)
+	return r
+}
+
+// Values returns the values slice from v. Panics v is not map.
+func Values(v interface{}) []interface{} {
+	_, r := Each(v)
+	return r
 }
