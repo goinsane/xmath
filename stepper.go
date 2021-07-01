f025f2d41c86fdd6592c86bbcadbb3a5161f0932
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-07-01 22:15:39 +0300

    improvements and bug fixes in Stepper

--- stepper.go
+++ stepper.go
@@ -19,13 +19,15 @@ var (
 )
 
 type Stepper struct {
-	stepRat   *big.Rat
-	stepNum   *big.Int
-	stepDenom *big.Int
-	maxRat    *big.Rat
-	minRat    *big.Rat
-	diffRat   *big.Rat
-	diffNum   *big.Int
+	stepRat       *big.Rat
+	stepNumHalf   *big.Int
+	stepDenomHalf *big.Int
+	stepNum       *big.Int
+	stepDenom     *big.Int
+	maxRat        *big.Rat
+	minRat        *big.Rat
+	diffRat       *big.Rat
+	diffNum       *big.Int
 }
 
 func NewStepper(step, max, min float64) (s *Stepper, err error) {
@@ -49,8 +51,16 @@ func NewStepper(step, max, min float64) (s *Stepper, err error) {
 		maxRat:  new(big.Rat).SetFloat64(max),
 		minRat:  new(big.Rat).SetFloat64(min),
 	}
-	s.stepNum, s.stepDenom = s.stepRat.Num(), s.stepRat.Denom()
+	s.stepNumHalf, s.stepDenomHalf = s.stepRat.Num(), s.stepRat.Denom()
+	//s.stepNum, s.stepDenom = s.stepRat.Num(), s.stepRat.Denom()
+	s.stepNum, s.stepDenom = new(big.Int).Mul(big.NewInt(2), s.stepNumHalf), new(big.Int).Mul(big.NewInt(2), s.stepDenomHalf)
 	s.diffRat = new(big.Rat).Sub(s.maxRat, s.minRat)
+	if q := new(big.Rat).Quo(s.diffRat, s.stepRat); !q.IsInt() {
+		f, _ := q.Float64()
+		if r := math.Remainder(f, 1); !IsZero(r) {
+			return nil, ErrStepperStepOverflow
+		}
+	}
 	if q, r := new(big.Int).QuoRem(s.stepDenom, s.diffRat.Denom(), new(big.Int)); r.Cmp(big.NewInt(0)) == 0 {
 		s.diffNum = new(big.Int).Mul(s.diffRat.Num(), q)
 	} else {
@@ -75,6 +85,19 @@ func (s *Stepper) Normalize(x float64) (float64, error) {
 			n.Add(n, big.NewInt(-1))
 		}
 	}
+	p, r := new(big.Int).QuoRem(n, s.stepNum, new(big.Int))
+	_ = p
+	n.Sub(n, r)
+	switch t, half := r.Sign(), s.stepNumHalf /*new(big.Int).Quo(s.stepNum, big.NewInt(2))*/ ; {
+	case t > 0:
+		if r.Cmp(half) >= 0 {
+			n.Add(n, s.stepNum)
+		}
+	case t < 0:
+		if r.Cmp(new(big.Int).Mul(big.NewInt(-1), half)) < 0 {
+			n.Sub(n, s.stepNum)
+		}
+	}
 	if n.Cmp(big.NewInt(0)) < 0 {
 		result, _ := s.minRat.Float64()
 		return result, ErrStepperMinExceeded
@@ -83,10 +106,19 @@ func (s *Stepper) Normalize(x float64) (float64, error) {
 		result, _ := s.maxRat.Float64()
 		return result, ErrStepperMaxExceeded
 	}
-	y := new(big.Rat).Add(s.minRat, new(big.Rat).SetFrac(n, s.stepDenom))
-	result, exact := y.Float64()
-	if !exact {
-		return result, ErrStepperInexactValue
+	result, exact := new(big.Rat).Add(s.minRat, new(big.Rat).SetFrac(n, s.stepDenom)).Float64()
+	if exact {
+		return result, nil
 	}
-	return result, nil
+	for i := 0; i < 2*(2+1); i++ {
+		j := i / 2
+		if i%2 >= 1 {
+			j = -j
+		}
+		k := new(big.Int).Add(n, big.NewInt(int64(j)))
+		if result2, exact2 := new(big.Rat).Add(s.minRat, new(big.Rat).SetFrac(k, s.stepDenom)).Float64(); exact2 {
+			return result2, nil
+		}
+	}
+	return result, ErrStepperInexactValue
 }
