d52c1a59472049d22a6887755183a7deb7a0adbc
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-07-12 19:47:35 +0300

    in Stepper; improved calculation of count and Count() method.

--- stepper.go
+++ stepper.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"math"
 	"math/big"
+	"math/bits"
 )
 
 var (
@@ -22,6 +23,9 @@ type Stepper struct {
 	stepNum *Number
 	maxNum  *Number
 	minNum  *Number
+	step    float64
+	max     float64
+	min     float64
 	count   int64
 }
 
@@ -34,23 +38,30 @@ func NewStepper(prec, base int, step, max, min float64) (s *Stepper, err error) 
 	s.stepNum = s.newNumber().SetFloat64(step)
 	if f, acc := s.stepNum.Float64(); f != step || acc != big.Exact {
 		return nil, ErrStepOverflow
+	} else {
+		s.step = f
 	}
 	s.maxNum = s.newNumber().SetFloat64(max)
 	if f, acc := s.maxNum.Float64(); f != max || acc != big.Exact || math.Nextafter(max, math.Inf(+1))-max >= step {
 		return nil, ErrMaxOverflow
+	} else {
+		s.max = f
 	}
 	s.minNum = s.newNumber().SetFloat64(min)
 	if f, acc := s.minNum.Float64(); f != min || acc != big.Exact || min-math.Nextafter(min, math.Inf(-1)) >= step {
 		return nil, ErrMinOverflow
+	} else {
+		s.min = f
 	}
-	n := RoundBigFloat(s.newNumber().Quo(s.newNumber().Sub(s.maxNum, s.minNum), s.stepNum).Float())
-	if !n.IsInt64() {
+	n := s.newNumber().Quo(s.newNumber().Sub(s.maxNum, s.minNum), s.stepNum)
+	count, acc := n.Int64()
+	if !n.IsInt() || acc != big.Exact {
 		return nil, ErrRangeOverflow
 	}
-	if n.Cmp(big.NewInt(0)) < 0 {
+	if count < 0 {
 		return nil, ErrUnorderedMaxMin
 	}
-	s.count = n.Int64()
+	s.count = count
 	return s, nil
 }
 
@@ -59,7 +70,7 @@ func (s *Stepper) newNumber() *Number {
 }
 
 func (s *Stepper) Count() int {
-	return int(s.count)
+	return int(s.count & int64(math.MaxInt64) >> (64 - bits.UintSize))
 }
 
 func (s *Stepper) Count64() int64 {
@@ -72,10 +83,10 @@ func (s *Stepper) Step(index int) (float64, error) {
 
 func (s *Stepper) Step64(index int64) (float64, error) {
 	if index >= s.count {
-		return math.NaN(), ErrMaxExceeded
+		return s.max, ErrMaxExceeded
 	}
 	if index < 0 {
-		return math.NaN(), ErrMinExceeded
+		return s.min, ErrMinExceeded
 	}
 	f, acc := s.newNumber().Add(s.minNum, s.newNumber().Mul(s.newNumber().SetInt64(index), s.stepNum)).Float64()
 	if acc != big.Exact {
@@ -94,6 +105,5 @@ func (s *Stepper) Normalize(f float64) (float64, error) {
 	if math.IsNaN(f) {
 		return math.NaN(), nil
 	}
-	n := RoundBigFloat(s.newNumber().Quo(s.newNumber().Sub(s.newNumber().SetFloat64(f), s.minNum), s.stepNum).Float())
-	return s.Step64(n.Int64())
+	return s.Step64(RoundBigFloat(s.newNumber().Quo(s.newNumber().Sub(s.newNumber().SetFloat64(f), s.minNum), s.stepNum).Float()).Int64())
 }
