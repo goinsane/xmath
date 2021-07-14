dbcf60f2c9ff7c9ec8a1e1a3eb467b9e190880d7
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-07-14 12:31:24 +0300

    improved Stepper examples

--- stepper_test.go
+++ stepper_test.go
@@ -18,6 +18,8 @@ func ExampleNewStepper() {
 	fmt.Println(err)
 	_, err = xmath.NewStepper(2, 10, 0.15, 3.01, 2.31)
 	fmt.Println(err)
+	_, err = xmath.NewStepper(2, 10, 0.1, 2.31, 3.01)
+	fmt.Println(err)
 
 	// Output:
 	// <nil>
@@ -25,6 +27,7 @@ func ExampleNewStepper() {
 	// max overflow
 	// min overflow
 	// range overflow
+	// unordered max min
 }
 
 func ExampleStepper_Step() {
@@ -45,4 +48,23 @@ func ExampleStepper_Step() {
 	// 2.81 <nil>
 	// 2.91 <nil>
 	// 3.01 <nil>
+}
+
+func ExampleStepper_Normalize() {
+	s, err := xmath.NewStepper(2, 10, 0.25, -5.00, -7.00)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(s.Normalize(0.50))
+	fmt.Println(s.Normalize(-7.75))
+	fmt.Println(s.Normalize(-6.376))
+	fmt.Println(s.Normalize(-6.375))
+	fmt.Println(s.Normalize(-6.374))
+
+	// Output:
+	// -5 max exceeded
+	// -7 min exceeded
+	// -6.5 <nil>
+	// -6.25 <nil>
+	// -6.25 <nil>
 }
