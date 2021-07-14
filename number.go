32f4c13c5767c24e45ae1c8ed9a73197acb07749
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-07-14 14:51:24 +0300

    implemented godoc for Number

--- number.go
+++ number.go
@@ -6,6 +6,11 @@ import (
 	"math/big"
 )
 
+// Number is a big.Float like implementation for integers and real numbers.
+// It rounds the big.Float to half away from zero after all of writing operations.
+// A Number can be created with new(Number) or NewNumber and etc.
+// A Number which is created by new(Number) has precision 0 and base 10.
+// Both of Precision and base, can't change after the Number created.
 type Number struct {
 	prec int
 	base int
@@ -13,6 +18,8 @@ type Number struct {
 	k    *big.Float
 }
 
+// NewNumber returns a new Number with given precision and base.
+// Calling the NewNumber, isn't necessary to create new Number.
 func NewNumber(prec, base int) *Number {
 	checkInvalidBase(base)
 	return &Number{
@@ -23,18 +30,22 @@ func NewNumber(prec, base int) *Number {
 	}
 }
 
+// NewBinary returns a new Number with base 2.
 func NewBinary(prec int) *Number {
 	return NewNumber(prec, 2)
 }
 
+// NewOctal returns a new Number with base 8.
 func NewOctal(prec int) *Number {
 	return NewNumber(prec, 8)
 }
 
+// NewDecimal returns a new Number with base 10.
 func NewDecimal(prec int) *Number {
 	return NewNumber(prec, 10)
 }
 
+// NewHexdecimal returns a new Number with base 16.
 func NewHexdecimal(prec int) *Number {
 	return NewNumber(prec, 16)
 }
@@ -48,36 +59,43 @@ func (z *Number) init() {
 	z.k = big.NewFloat(1)
 }
 
+// Prec returns precision of the Number.
 func (x *Number) Prec() int {
 	x.init()
 	return x.prec
 }
 
+// Base returns base of the Number.
 func (x *Number) Base() int {
 	x.init()
 	return x.base
 }
 
+// Float returns a copy of big.Float value.
 func (x *Number) Float() *big.Float {
 	x.init()
 	return new(big.Float).Copy(x.f)
 }
 
+// FloatMinPrec is similar with MinPrec method of big.Float.
 func (x *Number) FloatMinPrec() uint {
 	x.init()
 	return x.f.MinPrec()
 }
 
+// FloatMode is similar with Mode method of big.Float.
 func (x *Number) FloatMode() big.RoundingMode {
 	x.init()
 	return x.f.Mode()
 }
 
+// FloatPrec is similar with Prec method of big.Float.
 func (x *Number) FloatPrec() uint {
 	x.init()
 	return x.f.Prec()
 }
 
+// SetFloat is similar with Set except that x is big.Float.
 func (z *Number) SetFloat(x *big.Float) *Number {
 	z.init()
 	defer z.round()
@@ -85,6 +103,7 @@ func (z *Number) SetFloat(x *big.Float) *Number {
 	return z
 }
 
+// SetFloatMode is similar with SetMode method of big.Float.
 func (z *Number) SetFloatMode(mode big.RoundingMode) *Number {
 	z.init()
 	defer z.round()
@@ -92,6 +111,7 @@ func (z *Number) SetFloatMode(mode big.RoundingMode) *Number {
 	return z
 }
 
+// SetFloatPrec is similar with SetPrec method of big.Float.
 func (z *Number) SetFloatPrec(prec uint) *Number {
 	z.init()
 	defer z.round()
@@ -109,6 +129,7 @@ func (z *Number) round() {
 	z.f.Quo(z.f, z.k)
 }
 
+// Abs is similar with Abs method of big.Float.
 func (z *Number) Abs(x *Number) *Number {
 	z.init()
 	defer z.round()
@@ -116,16 +137,19 @@ func (z *Number) Abs(x *Number) *Number {
 	return z
 }
 
+// Acc is similar with Acc method of big.Float.
 func (x *Number) Acc() big.Accuracy {
 	x.init()
 	return x.f.Acc()
 }
 
+// Append is similar with Append method of big.Float.
 func (x *Number) Append(buf []byte, fmt byte, prec int) []byte {
 	x.init()
 	return x.f.Append(buf, fmt, prec)
 }
 
+// Add is similar with Add method of big.Float.
 func (z *Number) Add(x, y *Number) *Number {
 	z.init()
 	defer z.round()
@@ -133,11 +157,13 @@ func (z *Number) Add(x, y *Number) *Number {
 	return z
 }
 
+// Cmp is similar with Cmp method of big.Float.
 func (x *Number) Cmp(y *Number) int {
 	x.init()
 	return x.f.Cmp(y.f)
 }
 
+// Copy is similar with Copy method of big.Float.
 func (z *Number) Copy(x *Number) *Number {
 	z.init()
 	defer z.round()
@@ -145,62 +171,74 @@ func (z *Number) Copy(x *Number) *Number {
 	return z
 }
 
+// Float32 is similar with Float32 method of big.Float.
 func (x *Number) Float32() (float32, big.Accuracy) {
 	x.init()
 	return x.f.Float32()
 }
 
+// Float64 is similar with Float64 method of big.Float.
 func (x *Number) Float64() (float64, big.Accuracy) {
 	x.init()
 	return x.f.Float64()
 }
 
+// Format is similar with Format method of big.Float.
 func (x *Number) Format(s fmt.State, format rune) {
 	x.init()
 	x.f.Format(s, format)
 }
 
+// GobDecode is similar with GobDecode method of big.Float.
 func (z *Number) GobDecode(buf []byte) error {
 	z.init()
 	defer z.round()
 	return z.f.GobDecode(buf)
 }
 
+// GobEncode is similar with GobEncode method of big.Float.
 func (x *Number) GobEncode() ([]byte, error) {
 	x.init()
 	return x.f.GobEncode()
 }
 
+// Int is similar with Int method of big.Float.
 func (x *Number) Int(z *big.Int) (*big.Int, big.Accuracy) {
 	x.init()
 	return x.f.Int(z)
 }
 
+// Int64 is similar with Int64 method of big.Float.
 func (x *Number) Int64() (int64, big.Accuracy) {
 	x.init()
 	return x.f.Int64()
 }
 
+// IsInf is similar with IsInf method of big.Float.
 func (x *Number) IsInf() bool {
 	x.init()
 	return x.f.IsInf()
 }
 
+// IsInt is similar with IsInt method of big.Float.
 func (x *Number) IsInt() bool {
 	x.init()
 	return x.f.IsInt()
 }
 
+// MantExp is similar with MantExp method of big.Float.
 func (x *Number) MantExp(mant *Number) (exp int) {
 	x.init()
 	return x.f.MantExp(mant.f)
 }
 
+// MarshalText is similar with MarshalText method of big.Float.
 func (x *Number) MarshalText() (text []byte, err error) {
 	x.init()
 	return x.f.MarshalText()
 }
 
+// Mul is similar with Mul method of big.Float.
 func (z *Number) Mul(x, y *Number) *Number {
 	z.init()
 	defer z.round()
@@ -208,6 +246,7 @@ func (z *Number) Mul(x, y *Number) *Number {
 	return z
 }
 
+// Neg is similar with Neg method of big.Float.
 func (z *Number) Neg(x *Number) *Number {
 	z.init()
 	defer z.round()
@@ -215,6 +254,7 @@ func (z *Number) Neg(x *Number) *Number {
 	return z
 }
 
+// Parse is similar with Parse method of big.Float.
 func (z *Number) Parse(s string, base int) (n *Number, b int, err error) {
 	z.init()
 	defer z.round()
@@ -222,6 +262,7 @@ func (z *Number) Parse(s string, base int) (n *Number, b int, err error) {
 	return z, b, err
 }
 
+// Quo is similar with Quo method of big.Float.
 func (z *Number) Quo(x, y *Number) *Number {
 	z.init()
 	defer z.round()
@@ -229,17 +270,20 @@ func (z *Number) Quo(x, y *Number) *Number {
 	return z
 }
 
+// Rat is similar with Rat method of big.Float.
 func (x *Number) Rat(z *big.Rat) (*big.Rat, big.Accuracy) {
 	x.init()
 	return x.f.Rat(z)
 }
 
+// Scan is similar with Scan method of big.Float.
 func (z *Number) Scan(s fmt.ScanState, ch rune) error {
 	z.init()
 	defer z.round()
 	return z.f.Scan(s, ch)
 }
 
+// Set is similar with Set method of big.Float.
 func (z *Number) Set(x *Number) *Number {
 	z.init()
 	defer z.round()
@@ -247,6 +291,7 @@ func (z *Number) Set(x *Number) *Number {
 	return z
 }
 
+// SetFloat64 is similar with SetFloat64 method of big.Float.
 func (z *Number) SetFloat64(x float64) *Number {
 	z.init()
 	defer z.round()
@@ -254,6 +299,7 @@ func (z *Number) SetFloat64(x float64) *Number {
 	return z
 }
 
+// SetInf is similar with SetInf method of big.Float.
 func (z *Number) SetInf(signbit bool) *Number {
 	z.init()
 	defer z.round()
@@ -261,6 +307,7 @@ func (z *Number) SetInf(signbit bool) *Number {
 	return z
 }
 
+// SetInt is similar with SetInt method of big.Float.
 func (z *Number) SetInt(x *big.Int) *Number {
 	z.init()
 	defer z.round()
@@ -268,6 +315,7 @@ func (z *Number) SetInt(x *big.Int) *Number {
 	return z
 }
 
+// SetInt64 is similar with SetInt64 method of big.Float.
 func (z *Number) SetInt64(x int64) *Number {
 	z.init()
 	defer z.round()
@@ -275,6 +323,7 @@ func (z *Number) SetInt64(x int64) *Number {
 	return z
 }
 
+// SetMantExp is similar with SetMantExp method of big.Float.
 func (z *Number) SetMantExp(mant *Number, exp int) *Number {
 	z.init()
 	defer z.round()
@@ -282,6 +331,7 @@ func (z *Number) SetMantExp(mant *Number, exp int) *Number {
 	return z
 }
 
+// SetRat is similar with SetRat method of big.Float.
 func (z *Number) SetRat(x *big.Rat) *Number {
 	z.init()
 	defer z.round()
@@ -289,6 +339,7 @@ func (z *Number) SetRat(x *big.Rat) *Number {
 	return z
 }
 
+// SetString is similar with SetString method of big.Float.
 func (z *Number) SetString(s string) (n *Number, ok bool) {
 	z.init()
 	defer z.round()
@@ -296,6 +347,7 @@ func (z *Number) SetString(s string) (n *Number, ok bool) {
 	return z, ok
 }
 
+// SetUint64 is similar with SetUint64 method of big.Float.
 func (z *Number) SetUint64(x uint64) *Number {
 	z.init()
 	defer z.round()
@@ -303,16 +355,19 @@ func (z *Number) SetUint64(x uint64) *Number {
 	return z
 }
 
+// Sign is similar with Sign method of big.Float.
 func (x *Number) Sign() int {
 	x.init()
 	return x.f.Sign()
 }
 
+// Signbit is similar with Signbit method of big.Float.
 func (x *Number) Signbit() bool {
 	x.init()
 	return x.f.Signbit()
 }
 
+// Sqrt is similar with Sqrt method of big.Float.
 func (z *Number) Sqrt(x *Number) *Number {
 	z.init()
 	defer z.round()
@@ -320,11 +375,13 @@ func (z *Number) Sqrt(x *Number) *Number {
 	return z
 }
 
+// String is similar with String method of big.Float.
 func (x *Number) String() string {
 	x.init()
 	return x.f.String()
 }
 
+// Sub is similar with Sub method of big.Float.
 func (z *Number) Sub(x, y *Number) *Number {
 	z.init()
 	defer z.round()
@@ -332,16 +389,19 @@ func (z *Number) Sub(x, y *Number) *Number {
 	return z
 }
 
+// Text is similar with Text method of big.Float.
 func (x *Number) Text(format byte, prec int) string {
 	x.init()
 	return x.f.Text(format, prec)
 }
 
+// Uint64 is similar with Uint64 method of big.Float.
 func (x *Number) Uint64() (uint64, big.Accuracy) {
 	x.init()
 	return x.f.Uint64()
 }
 
+// UnmarshalText is similar with UnmarshalText method of big.Float.
 func (z *Number) UnmarshalText(text []byte) error {
 	z.init()
 	defer z.round()
