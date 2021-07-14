235a40efb0d13c50d311e131bdd6dfa189cde599
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-07-14 12:31:38 +0300

    improved Number examples

--- number_test.go
+++ number_test.go
@@ -6,6 +6,37 @@ import (
 	"github.com/goinsane/xmath"
 )
 
+func ExampleNumber() {
+	n := new(xmath.Number)
+	for i := 0; i < 20; i++ {
+		k := 0.25 * float64(i)
+		n.SetFloat64(k)
+		fmt.Println(n, k)
+	}
+
+	// Output:
+	// 0 0
+	// 0 0.25
+	// 1 0.5
+	// 1 0.75
+	// 1 1
+	// 1 1.25
+	// 2 1.5
+	// 2 1.75
+	// 2 2
+	// 2 2.25
+	// 3 2.5
+	// 3 2.75
+	// 3 3
+	// 3 3.25
+	// 4 3.5
+	// 4 3.75
+	// 4 4
+	// 4 4.25
+	// 5 4.5
+	// 5 4.75
+}
+
 func ExampleNewDecimal() {
 	n := xmath.NewDecimal(1)
 	for i := 0; i < 10; i++ {
